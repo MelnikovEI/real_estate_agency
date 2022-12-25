09c4334d29547c77b1e6ebe216a59bc9aa863001
EvgenyM <MelnikovEI@ya.ru>
2022-12-25 20:27:04 +0300

    normalize phone numbers to owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20, null=True)
-    owner_pure_phone = PhoneNumberField(blank=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
